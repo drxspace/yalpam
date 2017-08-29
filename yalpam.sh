14912c0c2d2f789d2c9535125c9c0501eb197fd0
John A Ginis <drxspace@gmail.com>
2017-08-29 11:15:44 +0300

    Reinstall/Update selected package

--- yalpam.sh
+++ yalpam.sh
@@ -20,30 +20,46 @@ export frunningPIDs=$(mktemp -u --tmpdir runningPIDs.XXXXXXXX)
 
 #export V=true			# V for verbose
 #	${V} && { echo "$1 $2 $3 $4" 1>&2; }
-
+# --close-on-unfocus
+ 
 declare -a runningPIDs=()
 
 # ---[ Functions ]-------------------------------------------------------------|
 
-doinstpkg() {
-	exec xterm -e "[[ \"$1\" == \"pacman\" ]] && { sudo $1 -Sy --force --noconfirm $2; } || { $1 -Sya --force --noconfirm $2; }"
+doscan4pkgs() {
+	echo -e '\f' >> "${fpipepkgssys}"
+	pacman -Qe |\
+		grep -vx "$(pacman -Qg base)" |\
+		grep -vx --line-buffered "$(pacman -Qm)" |\
+		awk '{printf "%d\n%s\n%s\n", ++i, $1, $2}' >> "${fpipepkgssys}"
+
+# | yad --progress --pulsate --auto-close --no-buttons --width=200 --borders=9 --center --align=center --skip-taskbar --title="One moment..." --text-align="center" --text="Querying packages..." 
+
+	echo -e '\f' >> "${fpipepkgslcl}"
+	pacman -Qm | awk '{printf "%d\n%s\n%s\n", ++i, $1, $2}' >> "${fpipepkgslcl}"
 	return
 }
-export -f doinstpkg
+export -f doscan4pkgs
 
 doreinstpkg() {
-	exec xterm -e "[[ \"$1\" == \"pacman\" ]] && { sudo $1 -Sy --force --noconfirm $2; } || { $1 -Sya --force --noconfirm $2; }"
+	xterm -e "[[ \"$1\" == \"pacman\" ]] && { sudo $1 -Sy --force --noconfirm $2; } || { $1 -Sya --force --noconfirm $2; }"
+	doscan4pkgs
 	return
 }
 export -f doreinstpkg
 
 doremovepkg() {
-	exec xterm -e "sudo $1 -Rsun $2"
-	export -f doscan4pkgs
+	xterm -e "sudo $1 -Rsun $2"
 	doscan4pkgs
 	return
 }
 export -f doremovepkg
+
+doinstpkg() {
+	doscan4pkgs
+	return
+}
+export -f doinstpkg
 
 doexecpkg() {
 	exec "$(which $1)" &>/dev/null
@@ -60,17 +76,17 @@ export -f domanpage
 dopopup() {
 	export manager=$1
 	export package=$3
-	yad	--form --width=400 --borders=9 --center --align=center --fixed \
+	export -f doscan4pkgs
+	yad	--form --width=400 --borders=9 --align=center --fixed \
 		--skip-taskbar --title="Choose action:" \
 		--image="dialog-information" --image-on-top \
-		--text="Please, choose your desired action from the list below by clicking on its elements" \
-		--field="<span color='#006699'>Reinstall selected package</span>!gtk-refresh":btn 'sh -c "doreinstpkg $manager $package"' \
-		--field="<span color='#006699'>Uninstall/Remove selected package</span>!gtk-delete":btn 'sh -c "doremovepkg $manager $package"' \
-		--field="<span color='#006699'>Install a package of the selected category</span>!gtk-refresh":btn 'sh -c "doreinstpkg $manager $package"' \
-		--field="":lbl \
-		--field="<span color='#006699'>Try to run selected package</span>!gtk-execute":btn 'sh -c "doexecpkg $package"' \
-		--field="<span color='#006699'>Try to view the man page of the selected package</span>!gtk-help":btn 'sh -c "domanpage $package"' \
-		--field="":lbl \
+		--text="Please, choose your desired action from the list below by clicking on its elements." \
+		--field="<span color='#006699'>Reinstall/Update selected package</span>!gtk-refresh":btn 'bash -c "doreinstpkg $manager $package"' \
+		--field="<span color='#006699'>Uninstall/Remove selected package</span>!gtk-delete":btn 'bash -c "doremovepkg $manager $package"' \
+		--field="<span color='#006699'>Install a package from the selected category</span>!gtk-go-down":btn 'bash -c "doinstpkg $manager"' \
+		--field="":lbl '' \
+		--field="<span color='#006699'>Try to run selected package</span>!gtk-execute":btn 'bash -c "doexecpkg $package"' \
+		--field="<span color='#006699'>Try to view the man page of the selected package</span>!gtk-help":btn 'bash -c "domanpage $package"' \
 		--buttons-layout="center" \
 		--button=$"Close!gtk-close!Closes the current dialog":0 &>/dev/null &
 
@@ -90,13 +106,13 @@ export -f dopopup
 # -----------------------------------------------------------------------------|
 
 doabout() {
-	yad	--form --width=400 --borders=9 --center --align=center --fixed \
+	yad	--form --width=400 --borders=9 --align=center --fixed \
 		--skip-taskbar --title="About Yet another Arch Linux PAckage Manager" \
 		--image="system-software-install" --image-on-top \
 		--text="<span font_size='medium' font_weight='bold'>Yet another Arch Linux PAckage Manager</span>\nby John A Ginis (a.k.a. <a href='https://github.com/drxspace'>drxspace</a>)\n<span font_size='small'>build on Summer of 2017</span>" \
-		--field="":lbl \
-		--field="These are packages from all enabled repositories except for base and base-devel ones. Also, you\'ll find packages that are locally installed such as AUR packages.":lbl \
-		--field="":lbl \
+		--field="":lbl '' \
+		--field="These are packages from all enabled repositories except for base and base-devel ones. Also, you\'ll find packages that are locally installed such as AUR packages.":lbl '' \
+		--field="":lbl '' \
 		--buttons-layout="center" \
 		--button=$"Close!gtk-close!Closes the current dialog":0 &>/dev/null &
 
@@ -127,21 +143,6 @@ dosavepkglists() {
 }
 export -f dosavepkglists
 
-doscan4pkgs() {
-	echo -e '\f' >> "${fpipepkgssys}"
-	pacman -Qe |\
-		grep -vx "$(pacman -Qg base)" |\
-		grep -vx --line-buffered "$(pacman -Qm)" |\
-		awk '{printf "%d\n%s\n%s\n", ++i, $1, $2}' >> "${fpipepkgssys}"
-
-# | yad --progress --pulsate --auto-close --no-buttons --width=200 --borders=9 --center --align=center --skip-taskbar --title="One moment..." --text-align="center" --text="Querying packages..." 
-
-	echo -e '\f' >> "${fpipepkgslcl}"
-	pacman -Qm | awk '{printf "%d\n%s\n%s\n", ++i, $1, $2}' >> "${fpipepkgslcl}"
-	return
-}
-export -f doscan4pkgs
-
 # -----------------------------------------------------------------------------|
 
 trap "rm -f ${fpipepkgssys} ${fpipepkgslcl} ${frunningPIDs};" EXIT
@@ -155,13 +156,13 @@ echo 'frmAboutPIDs=()
 frmPopupPIDs=()' > ${frunningPIDs}
 
 yad --plug="${fkey}" --tabnum=1 --list --grid-lines="hor" \
-    --dclick-action='sh -c "dopopup pacman %s %s %s"' \
+    --dclick-action='bash -c "dopopup pacman %s %s %s"' \
     --text "List of <i>system</i> packages:" \
     --search-column=2 --expand-column=2 --focus-field=1 \
     --column='No:':num --column='Package Name' --column='Package Version' <&3 &
 
 yad --plug="${fkey}" --tabnum=2 --list --grid-lines="hor" \
-    --dclick-action='sh -c "dopopup yaourt %s %s %s"' \
+    --dclick-action='bash -c "dopopup yaourt %s %s %s"' \
     --text "List of <i>local (includes AUR)</i> packages:" \
     --search-column=2 --expand-column=2 --focus-field=1 \
     --column='No:':num --column='Package Name' --column='Package Version' <&4 &
@@ -173,9 +174,9 @@ yad --key="${fkey}" --notebook --width=520 --height=640 \
     --text="<span font_size='medium' font_weight='bold'>View Lists of Installed Packages</span>\n\
 These are packages from all enabled repositories except for <i>base</i> repository. Also, you\'ll find packages that are locally installed such as <i>AUR</i> packages." \
     --tab=" <i>System</i> category" --tab=" <i>Local/AUR</i> category" \
-    --button="<span color='#0066ff'>List packages</span>!system-search!Scans databases for installed packages:sh -c 'doscan4pkgs'" \
-    --button="Save packages!gtk-save!Saves packages lists to disk:sh -c 'dosavepkglists'" \
-    --button="gtk-about:sh -c 'doabout'" \
+    --button="<span color='#0066ff'>List packages</span>!system-search!Scans databases for installed packages:bash -c 'doscan4pkgs'" \
+    --button="Save packages!gtk-save!Saves packages lists to disk:bash -c 'dosavepkglists'" \
+    --button="gtk-about:bash -c 'doabout'" \
     --button="gtk-close":0
 
 exec 3>&-
